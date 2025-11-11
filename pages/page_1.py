41c225b2279277525e06213b118dd35d1b0df837
S1161110 <s1161110@gm.ncue.edu.tw>
2025-11-11 11:12:53 +0000

    re

--- pages/page_1.py
+++ pages/page_1.py
@@ -14,38 +14,7 @@ def create_map():
     )
 
     metro_url="https://github.com/leoluyi/taipei_mrt/blob/master/routes.geojson"
-    building_style = {
-        "layers": [
-            {
-                "id": "Buildings",
-                "source": "buildings",
-                "source-layer": "building",
-                "type": "line",
-                "paint": {
-                    "line-color": "#ff0000",
-                    "line-width": 1,
-                },
-            },
-        ]
-    }
-    road_style = {
-        "layers": [
-            {
-                "id": "Roads",
-                "source": "transportation",
-                "source-layer": "segment",
-                "type": "line",
-                "paint": {
-                    "line-color": "#ffffff",
-                    "line-width": 2,
-                },
-            },
-        ]
-    }
-    m.add_pmtiles(
-        building_pmtiles, style=building_style, tooltip=True, fit_bounds=False
-    )
-    m.add_pmtiles(road_pmtiles, style=road_style, tooltip=True, fit_bounds=False)
+    m.add_geojson(metro_url, layer_name="台北捷運", tooltip=True)
     return m
 
 
