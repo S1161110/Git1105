0c9a5643205a102a61b73b8714f611d1bbe2ccc6
S1161110 <s1161110@gm.ncue.edu.tw>
2025-11-11 10:55:11 +0000

    try

--- pages/page_1.py
+++ pages/page_1.py
@@ -3,16 +3,7 @@ import leafmap.leafmap as leafmap
 
 @solara.component
 def Page():
-    # 建立地圖，以台北為中心
-    m = leafmap.Map(
-        center=(25.0330, 121.5654),  # 台北 101 附近
-        zoom=12,
-        basemap="CartoDB.DarkMatter"  # 暗色底圖
-    )
-
-    # 載入台北捷運路網（GeoJSON）
-    url = "https://raw.githubusercontent.com/leoluyi/taipei_mrt/refs/heads/master/routes.geojson"
+    m = leafmap.Map(center=(25.033, 121.5654), zoom=12, basemap="CartoDB.DarkMatter")
+    url = "https://raw.githubusercontent.com/datasets/taipei-metro/master/data/taipei_metro_lines.geojson"
     m.add_geojson(url, layer_name="台北捷運")
-
-    # ✅ 在 Solara 中顯示地圖
-    return solara.Display(m)
+    return m
