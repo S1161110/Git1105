abdf44e3b4fa34fb4bc9d29a6045030e95f7cf1c
S1161110 <s1161110@gm.ncue.edu.tw>
2025-11-11 06:12:59 +0000

    1111

--- pages/page_1.py
+++ pages/page_1.py
@@ -1,20 +1,21 @@
 import solara
-import leafmap.maplibregl as leafmap
-
-
-def create_map():
-
-    m = leafmap.Map(
-        style="liberty",
-        projection="globe",
-        height="750px",
-        zoom=2.5,
-        sidebar_visible=True,
-    )
-    return m
-
+import leafmap.leafmap as leafmap
 
 @solara.component
 def Page():
-    m = create_map()
-    return m.to_solara()
+    # 建立地圖，以台北為中心
+    m = leafmap.Map(
+        center=(25.0330, 121.5654),  # 台北 101 附近
+        zoom=12,
+        basemap="CartoDB.DarkMatter"  # 暗色底圖
+    )
+
+    # 載入台北捷運路網（GeoJSON）
+    # 你可以放自己的檔案或公開連結
+    url = "https://raw.githubusercontent.com/g0v/taipei-metro-data/master/metro-line.geojson"
+    m.add_geojson(url, layer_name="台北捷運")
+
+    # 在 Solara 中顯示地圖
+    solara.Figure(m)
+
+Page()
